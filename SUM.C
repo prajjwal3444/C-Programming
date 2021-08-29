b1839ac573842221ba6c7ef14adf0701108a4884
prajjwal3444 <89745702+prajjwal3444@users.noreply.github.com>
2021-08-29 23:52:41 +0530

    Add files via upload

---
+++ SUM.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int num1,num2,sum;
+clrscr();
+printf("Enter the value of num1\n");
+scanf("%d",&num1);
+printf("Enter the value of num2\n");
+scanf("%d",&num2);
+
+sum=num1+num2;
+printf("Sum of two numbers is=%d",sum);
+
+getch();
+}
