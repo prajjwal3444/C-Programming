5c0b769c62e9abb2f88d630affcdced8c91acda9
prajjwal3444 <89745702+prajjwal3444@users.noreply.github.com>
2021-08-29 23:57:49 +0530

    Add files via upload

---
+++ MUL.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,mul;
+clrscr();
+printf("Enter the value of num1\n");
+scanf("%d",&num1);
+printf("Enter the value of num2\n");
+scanf("%d",&num2);
+mul=num1*num2;
+printf("mul of num1 and num2 =%d",mul);
+getch();
+}
