63b1272f817730bd3c7d178188ffeab8f008f360
prajjwal3444 <89745702+prajjwal3444@users.noreply.github.com>
2021-08-30 20:13:06 +0530

    Add files via upload

---
+++ SUB.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,sub;
+clrscr();
+printf("Enter the value of num1\n");
+scanf("%d",&num1);
+printf("Enter the value of num2\n");
+scanf("%d",&num2);
+sub=num1-num2;
+printf("sub of num1 and num2 =%d",sub);
+getch();
+}
