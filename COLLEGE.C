c3e4f9833b890517521897853579be6367b3cafa
prajjwal3444 <89745702+prajjwal3444@users.noreply.github.com>
2022-04-18 05:59:50 +0530

    Add files via upload

---
+++ COLLEGE.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int x,y,z,p,r,t,SI,addition,subtraction,multiply,divide;
+//printf("enter the value of p");
+//scanf("%d",&p);
+//printf("enter the value of r");
+//scanf("%d",&r);
+//printf("enter the value of t");
+//scanf("%d",&t);
+//SI=(p*r*t)/t;
+//x=10;
+//y=20;
+//z=x+y;
+//printf("z=%d",z);
+//printf("Namaste duniya");
+//printf("SI=%d",SI);
+printf("enter the value of x");
+scanf("%d",&x);
+printf("enter the value of y");
+scanf("%d",&y);
+addition=x+y;
+subtraction=x-y;
+multiply=x*y;
+divide=x/y;
+printf("addition=%d\n",addition);
+printf("subtraction=%d\n",subtraction);
+printf("multiply=%d\n",multiply);
+printf("divide=%d",divide);
+getch();
+}
